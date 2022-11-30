ce0fb0caade97627ae6d2e2c9f6f783210578cfc
artipavl <40296816+artipavl@users.noreply.github.com>
2022-11-30 23:16:35 +0200

    Додає хуки

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { React, Component } from 'react';
+import { useState, useEffect } from 'react';
 import { ImageGallery } from './ImageGallery/ImageGallery';
 import { Searchbar } from './Searchbar/Searchbar';
 import { getImage } from '../Api/Api';
@@ -6,95 +6,65 @@ import { Loader } from './Loader/Loader';
 import { Button } from './Button/Button';
 import { Modal } from './Modal/Modal';
 
-// import { nanoid } from 'nanoid';
+export function App() {
+  const [images, setImages] = useState([]);
+  const [loade, setLoade] = useState(false);
+  const [page, setPage] = useState(0);
+  const [name, setName] = useState('');
+  const [search, setSearch] = useState('');
+  const [src, setSrc] = useState('');
+  const [alt, setAlt] = useState('');
 
-export class App extends Component {
-  state = {
-    images: [],
-    loade: false,
-    page: 1,
-    name: '',
-    search: '',
-  };
-
-  componentDidUpdate(_, prevState) {
-    if (
-      prevState.name !== this.state.name ||
-      prevState.page !== this.state.page
-    ) {
-      getImage(this.state.name, this.state.page)
-        .then(({ hits }) => {
-          const data = hits.map(
-            ({ id, webformatURL, largeImageURL, tags }) => ({
-              id,
-              webformatURL,
-              largeImageURL,
-              tags,
-            })
-          );
-          this.setState(state => ({
-            images: [...state.images, ...data],
-            loade: false,
-          }));
-        })
-        .catch(console.error);
+  useEffect(() => {
+    if (page === 0 && !name) {
+      return;
     }
-  }
+    setLoade(true);
+    getImage(name, page)
+      .then(({ hits }) => {
+        const data = hits.map(({ id, webformatURL, largeImageURL, tags }) => ({
+          id,
+          webformatURL,
+          largeImageURL,
+          tags,
+        }));
+        setImages(images => [...images, ...data]);
+        setLoade(false);
+      })
+      .catch(console.error);
+  }, [name, page]);
 
-  onSearch = name => {
+  const onSearch = searchName => {
     //перевірка на однаковий пошук
-    if (name!==this.state.name)
-      this.setState(state => ({
-        images: [],
-        loade: true,
-        page: 1,
-        name: state.search,
-      }));
+    if (searchName !== name) {
+      setName(searchName);
+
+      setPage(1);
+      setImages([]);
+    }
   };
 
-  onChangeSearch = search => {
-    this.setState({
-      search,
-    });
+  const loadeMore = () => {
+    setPage(page => page + 1);
   };
 
-  loadeMore = () => {
-    this.setState(state => ({
-      loade: true,
-      page: state.page + 1,
-    }));
+  const openModal = (src, alt) => {
+    setSrc(src);
+    setAlt(alt);
   };
 
-  openModal = (src, alt) => {
-    this.setState({
-      src,
-      alt,
-    });
+  const closeModal = () => {
+    setSrc('');
+    setAlt('');
   };
 
-  closeModal = () => {
-    this.setState({
-      src: '',
-      alt: '',
-    });
-  };
-
-  render() {
-    const { search, images, loade, src, alt } = this.state;
-    return (
-      <div className="App">
-        <Searchbar
-          onSearch={this.onSearch}
-          onChange={this.onChangeSearch}
-          search={search}
-        />
-        {images.length > 0 && (
-          <ImageGallery images={images} click={this.openModal} />
-        )}
-        {loade && <Loader />}
-        {images.length > 0 && <Button onClick={this.loadeMore} />}
-        {src && <Modal close={this.closeModal} src={src} alt={alt} />}
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <Searchbar onSearch={onSearch} onChange={setSearch} search={search} />
+      {images.length > 0 && <ImageGallery images={images} click={openModal} />}
+      {loade && <Loader />}
+      {images.length > 0 && <Button onClick={loadeMore} />}
+      {src && <Modal close={closeModal} src={src} alt={alt} />}
+    </div>
+  );
 }
