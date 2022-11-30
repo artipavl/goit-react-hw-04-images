4b8458f360e19c1809a50e1013347e3c36e3cc6a
artipavl <40296816+artipavl@users.noreply.github.com>
2022-11-30 23:16:48 +0200

    Додає хуки

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,40 +1,37 @@
-import { Component } from 'react';
+import { useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 import css from './Modal.module.css';
 
-export class Modal extends Component {
-  static propTypes = {
-    src: PropTypes.string.isRequired,
-    alt: PropTypes.string.isRequired,
-    close: PropTypes.func.isRequired,
-  };
-
-  componentDidMount() {
-    window.addEventListener('keydown', this.keyDown);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.keyDown);
-  }
-
-  keyDown = e => {
-    if (e.code === 'Escape') {
-      this.props.close();
+export function Modal({ src, alt, close }) {
+  useEffect(() => {
+    function keyDown(e) {
+      if (e.code === 'Escape') {
+        close();
+      }
     }
-  };
 
-  render() {
-    const { src, alt, close } = this.props;
-    return (
-      <div
-        className={css.Overlay}
-        onClick={e => e.target === e.currentTarget && close()}
-      >
-        <div className={css.Modal}>
-          <img src={src} alt={alt} />
-        </div>
+    window.addEventListener('keydown', keyDown);
+
+    return () => {
+      window.removeEventListener('keydown', keyDown);
+    };
+  }, [close]);
+
+  return (
+    <div
+      className={css.Overlay}
+      onClick={e => e.target === e.currentTarget && close()}
+    >
+      <div className={css.Modal}>
+        <img src={src} alt={alt} />
       </div>
-    );
-  }
+    </div>
+  );
 }
+
+Modal.propTypes = {
+  src: PropTypes.string.isRequired,
+  alt: PropTypes.string.isRequired,
+  close: PropTypes.func.isRequired,
+};
